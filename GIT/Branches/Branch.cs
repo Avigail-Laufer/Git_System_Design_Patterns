c0d100346fe03f709865e51b01c327b9d418b2ed
לאופר אביגיל צירל <  @mbjcomp.local>
2024-03-26 16:00:11 +0200

    first

---
+++ GIT/Branches/Branch.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GIT.Branches
+{
+    internal class Branch:ICloneable
+    {
+        #region proprty
+        public string Name { get; set; }
+        public DateTime ManufacturingDate { get; set; }
+        public double Size { get; set; }
+        //public list<fileSystem> GeneralFile { get; set; }
+        public Branch() { }
+
+
+        #endregion
+
+        #region function
+        public object Clone()
+        {
+            throw new NotImplementedException();
+        }
+        public  Boolean Delete()
+        {
+            return true;
+        }
+        public Boolean Merge()
+        {
+            return false;
+        }
+        public void Review()
+        {
+
+        }
+        public void Commit()
+        {
+
+        }
+        #endregion
+    }
+
+
+}
---
+++ GIT/Command/CommitCommand.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GIT.Command
+{
+    internal class CommitCommand : CommandsOnBranch
+    {
+        #region function
+        public override void excute()
+        {
+            throw new NotImplementedException();
+        }
+        #endregion
+    }
+}
